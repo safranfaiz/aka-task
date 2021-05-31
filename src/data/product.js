bab926f9c2bdd681ab2dc86a71215d210a112dce
Mohamed Safran <safranfaiz@yahoo.com>
2021-06-01 00:24:19 +0530

    product data adde

---
+++ src/data/product.js
@@ -0,0 +1,75 @@
+export const productData = [
+  {
+    brand: "Puglia Pop",
+    name: "Luminaria Primitivo di Manduria 2019",
+    location: {
+      image: "//images.vivino.com/labels/XA7tSWkTSieTPdwnx7qIqw.jpg",
+      countryCode: "it",
+      countryState: "Primitivo di Manduria",
+      countryName: "Italy"
+
+    },
+    price: {
+      amount: "24.95",
+      code: "€"
+    },
+    rating: {
+      point: 4.7,
+      count: 30
+    },
+    userRating: {
+      type: "rating",
+      point: 5.0,
+      description: "Absolutely fabulous! Smooth, delicious notes of plum, cherry, vanilla and tobacco. Thoroughly enjoyed it and the bottle is so cute!",
+      img: "",
+      userName: "Michaela 3 (28 ratings)",
+      postDate: "Mar 23, 2021"
+    }
+  },
+  {
+    brand: "Luccarelli",
+    name: "Old Vines Primitivo di Manduria 2016",
+    location: {
+      image: "//images.vivino.com/labels/XA7tSWkTSieTPdwnx7qIqw.jpg",
+      countryCode: "it",
+      countryState: "Primitivo di Manduria",
+      countryName: "Italy"
+
+    },
+    price: {
+      amount: "29.95",
+      code: "€"
+    },
+    rating: {
+      point: 4.5,
+      count: 5555
+    },
+    userRating: {
+      type: "none"
+    }
+  },
+  {
+    brand: "Château Purcari",
+    name: "Negru de Purcari 2018",
+    location: {
+      image: "//images.vivino.com/labels/XA7tSWkTSieTPdwnx7qIqw.jpg",
+      countryCode: "md",
+      countryState: "South Eastern",
+      countryName: "Moldova"
+
+    },
+    price: {
+      amount: "23.95",
+      code: "€"
+    },
+    rating: {
+      point: 4.5,
+      count: 481
+    },
+    userRating: {
+      type: "text",
+      description: "This vintage rates better than any other year for this wine",
+      img: ""
+    }
+  }
+]
